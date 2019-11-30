e988ef5700bbdda229f4ab27cee3537992e9b184
Nasif Shah <33353457+nasifshah2017@users.noreply.github.com>
2019-11-29 21:30:16 -0400

    Add files via upload

--- jQuery-SelectorsExercise.js
+++ jQuery-SelectorsExercise.js
@@ -18,7 +18,7 @@ $("div").css("background", "purple");
 
 $(".highlight").css("width", "200px");
 
-$("third").css("2px solid orange");
+$("#third").css("border", "2px solid orange");
 
 $("div:first").css("color", "pink");
 
